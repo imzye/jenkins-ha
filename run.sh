106b5a86bc3ee9f72f8919350b7f61f346ac1dcf
Yi Zhang <guhongliuying@qq.com>
2020-07-30 22:23:25 +0800

    format doc

--- run.sh
+++ run.sh
@@ -9,44 +9,44 @@ MASTER_IP=""
 ## startup
 function rand_ip() {
   min=1
-  max=$(echo $ETCD_IP_LIST|awk '{print NF}')
-  num=$(($RANDOM+1000000000))
-  rand=$(($num%$max+$min))
-  echo $ETCD_IP_LIST |cut -d" " -f$rand
+  max=$(echo $ETCD_IP_LIST | awk '{print NF}')
+  num=$(($RANDOM + 1000000000))
+  rand=$(($num % $max + $min))
+  echo $ETCD_IP_LIST | cut -d" " -f$rand
 }
 
 function self_reg() {
-  rc=$(curl -s $ETCD_IP:$ETCD_PORT/v2/keys/$KEY_NAME -XPUT -d value="$EP" -d prevExist="false" -d ttl=$TTL|jq '.["errorCode"]')
+  rc=$(curl -s $ETCD_IP:$ETCD_PORT/v2/keys/$KEY_NAME -XPUT -d value="$EP" -d prevExist="false" -d ttl=$TTL | jq '.["errorCode"]')
 }
 
 function get_master_ip() {
-  MASTER_IP=$(curl -s $ETCD_IP:$ETCD_PORT/v2/keys/$KEY_NAME |jq '.["node"]["value"]')
+  MASTER_IP=$(curl -s $ETCD_IP:$ETCD_PORT/v2/keys/$KEY_NAME | jq '.["node"]["value"]')
 }
 
 function refresh_ttl() {
   x=$(curl -s $ETCD_IP:$ETCD_PORT/v2/keys/$KEY_NAME -XPUT -d ttl=$TTL -d refresh=true -d prevExist=true)
 }
 
-while :;do
-ETCD_IP=$(rand_ip)
-self_reg
-if [[ $rc -eq 105 ]];then
-  get_master_ip
-  echo "[INFO] `date '+%Y-%m-%d %H:%M:%S'` MASTER_IP: $MASTER_IP"
-  if [[ "$MASTER_IP" == "\"$EP\"" ]];then
-    echo "[INFO] `date '+%Y-%m-%d %H:%M:%S'` keep jenkins start"
-    service jenkins start
-    refresh_ttl
-  else
-    echo "[INFO] `date '+%Y-%m-%d %H:%M:%S'` service jenkins stop"
-    service jenkins stop
-    echo "[INFO] `date '+%Y-%m-%d %H:%M:%S'` sync data from $MASTER_IP"
-    rsync -a ${MASTER_IP//\"/}::jenkins/var/lib/jenkins /var/lib/
-    echo "[INFO] `date '+%Y-%m-%d %H:%M:%S'` data sync done"
+while :; do
+  ETCD_IP=$(rand_ip)
+  self_reg
+  if [[ $rc -eq 105 ]]; then
+    get_master_ip
+    echo "[INFO] $(date '+%Y-%m-%d %H:%M:%S') MASTER_IP: $MASTER_IP"
+    if [[ "$MASTER_IP" == "\"$EP\"" ]]; then
+      echo "[INFO] $(date '+%Y-%m-%d %H:%M:%S') keep jenkins start"
+      service jenkins start
+      refresh_ttl
+    else
+      echo "[INFO] $(date '+%Y-%m-%d %H:%M:%S') service jenkins stop"
+      service jenkins stop
+      echo "[INFO] $(date '+%Y-%m-%d %H:%M:%S') sync data from $MASTER_IP"
+      rsync -a ${MASTER_IP//\"/}::jenkins/var/lib/jenkins /var/lib/
+      echo "[INFO] $(date '+%Y-%m-%d %H:%M:%S') data sync done"
+    fi
+  elif [[ "$rc" == "null" ]]; then
+    echo "[INFO] $(date '+%Y-%m-%d %H:%M:%S') register success"
+    continue
   fi
-elif [[ "$rc" == "null" ]];then
-  echo "[INFO] `date '+%Y-%m-%d %H:%M:%S'` register success"
-  continue
-fi
-sleep $HBT
+  sleep $HBT
 done
